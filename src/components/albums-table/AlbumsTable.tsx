1202cb5ebd08cedd7e0474799a33606f1c5bf88e
hiroji <joegilbertdev@outlook.com>
2025-08-02 20:48:03 +0100

    Filters agGrid rowData by owner

--- src/components/albums-table/AlbumsTable.tsx
+++ src/components/albums-table/AlbumsTable.tsx
@@ -1,19 +1,24 @@
-import { useAlbumRows} from "../../api";
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
-import { AgGridReact } from 'ag-grid-react';
+import {useAlbumRows} from "../../api";
+import {AllCommunityModule, ModuleRegistry} from 'ag-grid-community';
+import {AgGridReact} from 'ag-grid-react';
 import {tableContainerStyle} from "./styles";
 import {getColumnDefinitions} from "./get-column-definitions.ts";
+import {Owners, useOwnerContext} from "../../../owner-context";
+import {useMemo} from "react";
 
 export const AlbumsTable = () => {
     // Register all Community features
     ModuleRegistry.registerModules([AllCommunityModule]);
 
     const {data} = useAlbumRows();
+    const {owner} = useOwnerContext();
+
+    const rowData = useMemo( () => owner !== Owners.All ? data?.filter((album) => album.owner === owner) ?? [] : data ?? [], [data, owner]);
 
     return (
         <div className={tableContainerStyle}>
             <AgGridReact
-                rowData={data}
+                rowData={rowData}
                 columnDefs={getColumnDefinitions()}
             />
         </div>
